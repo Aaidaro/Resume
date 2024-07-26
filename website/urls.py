9bef48d3e3dfe97a9fb899618db0705bdd0af57e
Aaidaro <aaidaroshani@gmail.com>
2024-07-26 22:28:42 +0330

    Update url tags
    url tags in the project need to be updated to apps name + url

--- website/urls.py
+++ website/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from website.views import *
 
+app_name = 'website'
+
 urlpatterns = [
     path('', about_view, name= "about"),
     path('resume/', resume_view, name= "resume"),
