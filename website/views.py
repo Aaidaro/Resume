e159484cc61ff15103e6390f01e039df44a3e91c
Aaidaro <aaidaroshani@gmail.com>
2024-07-27 12:23:20 +0330

    Update urls and templates
    Set dynamic context via render in views

--- website/views.py
+++ website/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def about_view(request):
-    return render(request, 'website/about.html')
+    context = {'name': 'Aida Roshani', 'job_title1': 'Software Engineer',
+               'job_title2': 'Data Scientist', 'github_link':'https://github.com/Aaidaro',
+               'linkedin_link': 'https://www.linkedin.com/in/aida-roshani-73b489181/',}
+    
+    return render(request, 'website/about.html', context)
 
 def resume_view(request):
     return render(request, 'website/resume.html')
@@ -11,4 +16,8 @@ def project_view(request):
     return render(request, 'website/projects.html')
 
 def project_main_view(request):
-    return render(request, 'website/project_main.html')
+    context = {'developers_name': 'Aida Roshani',
+               "project_link_github":"https://github.com/Aaidaro/Resume",
+               "project_link_preview":"github.com/Aaidaro/Resume",
+               "date":"27 July, 2022",}
+    return render(request, 'website/project_main.html', context)
