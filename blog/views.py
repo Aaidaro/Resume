fe653c19b3ac12b628a5aed9bc522a88d3b4ba57
Aaidaro <aaidaroshani@gmail.com>
2024-08-09 16:56:08 +0330

    register model in the view

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from blog.models import Post
+from datetime import datetime
 
 def blog_main_view(request):
-    return render(request, 'blog/blog_main.html')
+    published_posts = Post.objects.filter(status=1, published_date__lte=datetime.now())
+    draft_posts = Post.objects.filter(status= 0)
+    context = {'published_posts':published_posts, "draft_posts":draft_posts}
+    return render(request, 'blog/blog_main.html', context)
 
-def single_blog_view(request):
-    return render(request, 'blog/blog_single.html')
+def single_blog_view(request, pid):
+    post = get_object_or_404(Post, pk=pid)
+    context = {'post':post}
+    return render(request, 'blog/blog_single.html', context)
+
