a76d8e3344f694670ac341bf806cd0a34a32fa54
Aaidaro <aaidaroshani@gmail.com>
2024-08-11 20:34:52 +0330

    Update

--- website/admin.py
+++ website/admin.py
@@ -1,7 +1,1 @@
 from django.contrib import admin
-from website.models import Project
-
-class ProjectAdmin(admin.ModelAdmin):
-    date_hierarchy = "created_date"
-    list_display = ["title", "developer", "Link", "created_date"]
-admin.site.register(Project,ProjectAdmin)
--- website/views.py
+++ website/views.py
@@ -12,8 +12,4 @@ def project_view(request):
     return render(request, 'website/projects.html')
 
 def project_single_view(request):
-    context = {'developer': 'Aida Roshani',
-               "project_link_github":"https://github.com/Aaidaro/Resume",
-               "link_preview":"github.com/Aaidaro/Resume",
-               "date":"27 July, 2022",}
-    return render(request, 'website/project_main.html', context)
+    return render(request, 'website/project_main.html')
